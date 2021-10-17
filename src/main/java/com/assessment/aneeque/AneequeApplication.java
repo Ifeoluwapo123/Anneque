7084bee41abb27a1761529cb69752e2c7b2a2e08
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-10-17 22:15:01 +0100

    entry class

---
+++ src/main/java/com/assessment/aneeque/AneequeApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.aneeque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AneequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AneequeApplication.class, args);
+    }
+
+}
