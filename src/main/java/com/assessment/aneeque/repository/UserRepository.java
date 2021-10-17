14b40b908010d5199003db04c254e136ce3ab522
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-10-17 22:22:15 +0100

    user repository

---
+++ src/main/java/com/assessment/aneeque/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.assessment.aneeque.repository;
+
+import com.assessment.aneeque.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByPassword(String password);
+}
